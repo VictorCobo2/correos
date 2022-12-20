f976a8dde4c672070c7eb0fc3fea2dc1ccc553f4
Cristian David Hernandez Suarez <69637534+cristian-hernandez20@users.noreply.github.com>
2022-12-19 19:12:52 -0500

    cambios

--- src/routes/correos.routes.ts
+++ src/routes/correos.routes.ts
@@ -1,6 +1,8 @@
-import express from 'express'
-import { enviarCorreo } from '../controllers/correo.controller';
+import express from "express";
+import { enviarCorreo, enviarCorreoAzul, enviarCorreoAmarillo } from "../controllers/correo.controller";
 
 export const correo_route = express.Router();
 
 correo_route.post("/correo", enviarCorreo);
+correo_route.post("/correo1", enviarCorreoAzul);
+correo_route.post("/correo2", enviarCorreoAmarillo);
